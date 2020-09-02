e7c4f043c01cf6d8c755287bf7725ca892bfbf41
Евгений Евгеньевич Бученков <eugenebuch@niuitmo.ru>
2020-09-02 20:32:03 +0300

    Добавьте файлы проекта.

---
+++ XML_Parser/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XML_Parser
+{
+    class Model
+    {
+        public int id, bid;
+        public int? cbid;
+        public string Cbid { 
+            set 
+            { 
+                if (value == null)
+                {
+                    cbid = null;
+                }
+                else
+                {
+                    cbid = int.Parse(value);
+                }
+            } 
+        }
+        public bool available;
+        public string type;
+    }
+}
---
+++ XML_Parser/Program.cs
@@ -0,0 +1,63 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace XML_Parser
+{
+    class Program
+    {        
+        static string GetCode(string url)
+        {
+            string code;
+            using (var client = new WebClient())
+            {
+                client.Encoding = Encoding.GetEncoding("windows-1251");
+                code = client.DownloadString(url);
+            }
+            return code;
+        }
+
+        static void Main(string[] args)
+        {
+            Handler();
+        }
+
+        static async void Handler()
+        {
+            var url = @"https://yastatic.net/market-export/_/partner/help/YML.xml";
+            var config = Configuration.Default;
+            var context = BrowsingContext.New(config);
+
+            var source = GetCode(url);
+
+            var document = await context.OpenAsync(req => req.Content(source));
+            var offersList = document.All.Where(elem => elem.LocalName == "offer");
+            List<Model> offersAsModels = new List<Model>();
+            foreach (var item in offersList)
+            {
+                // Прямое обозначение переменных, на мой взгляд, выглядит более удобочитаемее, чем через конструктор
+                offersAsModels.Add(new Model() { 
+                    id = int.Parse(item.GetAttribute("id")),
+                    bid = int.Parse(item.GetAttribute("bid")),
+                    Cbid = item.GetAttribute("cbid"),
+                    type = item.GetAttribute("type"),
+                    available = bool.Parse(item.GetAttribute("available"))
+                });
+            }
+
+            // ../XML_Parser/bin/Debug/netcoreapp3.1
+            using (var txt = new StreamWriter("output.txt"))
+            {
+                foreach (var item in offersAsModels)
+                {
+                    txt.WriteLine(item.id);
+                }
+            }
+
+        }
+    }
+}
